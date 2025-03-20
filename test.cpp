57b3c6b7c977b4db7fb27c5c69ebe6dffe66d4ef
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2025-03-20 10:58:19 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,1 +1,12 @@
-import HBCF;
+import hbcf
+import <fstream>
+import <iostream>
+import <vector>
+using namespace std;
+
+int main() {
+  ifstream in("FileReadExample.cpp");
+  string example((istreambuf_iterator<char>(in)), 
+    istreambuf_iterator<char>());
+  cout << GetHitbox_Vector(example.c_str())
+}
