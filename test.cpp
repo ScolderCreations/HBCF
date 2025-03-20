6aa607bf72943bd138b696f4c750bb32e2df9e65
Scolder <69083943+ScolderCreations@users.noreply.github.com>
2025-03-20 18:37:17 +0000

    changes because i forgor

--- test.cpp
+++ test.cpp
@@ -1,13 +1,14 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
 import hbcf
-import <fstream>
-import <iostream>
-import <vector>
+
 using namespace std;
 
 int main() {
   ifstream in("example/example.txt");
   string example((istreambuf_iterator<char>(in)), 
     istreambuf_iterator<char>());
-  cout << GetHitbox_Vector(example.c_str())
-  return 1
+  cout << GetHitbox_Vector(example.c_str());
+  return 1;
 }
